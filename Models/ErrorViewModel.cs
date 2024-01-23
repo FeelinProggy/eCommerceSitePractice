d64194ce23576badf1852aa425dd140107cdbb68
Jennifer Peterson <jennpeterson986@gmail.com>
2024-01-23 14:27:22 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eCommerceSitePractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
