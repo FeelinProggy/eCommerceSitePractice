4a99e5bdb05df99ca81272fc8ba8a438e6526142
Jennifer Peterson <jennpeterson986@gmail.com>
2024-03-12 15:48:41 -0700

    Create product fully functional Added some styling

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSitePractice.Data;
+using eCommerceSitePractice.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerceSitePractice.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly ProductContext _context;
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                ViewData["Message"] = $"{product.Name} was added successfully!";
+                return View();
+            }
+            return View(product);
         }
     }
 }
