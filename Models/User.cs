f8f5d1e8b71abe008ad42c4488edbeff0bc1868f
Jennifer Peterson <jennpeterson986@gmail.com>
2024-03-14 16:10:02 -0700

    Added Register View

--- Models/User.cs
+++ Models/User.cs
@@ -32,11 +32,13 @@ namespace eCommerceSitePractice.Models
 
         [Required]
         [StringLength(75, MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
         [Compare(nameof(Password))]
         [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
     }
 }
---
+++ Views/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSitePractice.Views
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
