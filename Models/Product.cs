7f3e7de8c9fb03e4805e9077363e2f0d50186fa1
Jennifer Peterson <jennpeterson986@gmail.com>
2024-03-15 10:31:10 -0700

    Added CartProductViewModel for shopping cart cookie

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
 
 namespace eCommerceSitePractice.Models
 {
@@ -35,6 +36,22 @@ namespace eCommerceSitePractice.Models
         /// </summary>
         [Range(0,200)]
         public double Price { get; set; }
+        
+    }
+
+    /// <summary>
+    /// Represents a product that has been added
+    /// to the users shopping cart cookie
+    /// </summary>
+    public class CartProductViewModel
+    {
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+
         
     }
 }
