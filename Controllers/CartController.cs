9046d24da53772f91f6d6c6df85a2de3c7a5828b
Jennifer Peterson <jennpeterson986@gmail.com>
2024-03-14 23:21:27 -0700

    Add to shopping cart button added to Catalog

---
+++ Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using eCommerceSitePractice.Data;
+using eCommerceSitePractice.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSitePractice.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public CartController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            Product? productToAdd = _context.Products.Where(p => p.ProductID == id).SingleOrDefault();
+
+            if (productToAdd == null)
+            {
+                // Product not found
+                TempData["Message"] = "That item is no longer in stock";
+                return RedirectToAction("Index", "Games");
+            }
+
+
+            TempData["Message"] = "Item added to cart!";
+            return RedirectToAction("Index", "Games");
+        }
+    }
+}
